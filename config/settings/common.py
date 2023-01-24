ba72c62bc615b3ac4751b9473f290d518a3ec469
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-01-24 06:15:04 -0500

    Change 'static' to 'staticfiles'.

--- config/settings/common.py
+++ config/settings/common.py
@@ -108,8 +108,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_DIR = BASE_DIR / 'static'
+STATIC_URL = '/staticfiles/'
+STATIC_DIR = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
