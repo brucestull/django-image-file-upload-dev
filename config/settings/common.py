f775150009c521c0b244e5f169f4dece6aa7dc92
Bruce Stull <47562501+brucestull@users.noreply.github.com>
2023-01-24 06:38:03 -0500

    In progress

--- config/settings/common.py
+++ config/settings/common.py
@@ -111,8 +111,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_DIR = BASE_DIR / 'static'
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
